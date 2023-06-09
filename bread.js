65b842b1f30ddb1a3c8a8ba69cd46259f73f47c7
Rifqi <mohamadrifki66@gmail.com>
2023-06-09 16:09:33 +0700

    c19 blom selesai

--- bread.js
+++ bread.js
@@ -1,14 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const fs = require('node:fs');
-import  sqlite3  from 'sqlite3';
+const sqlite3 = require('sqlite3').verbose();
 
 const app = express()
-
-let rawdata = fs.readFileSync('data.json');
-let bread = JSON.parse(rawdata)
-
 const port = 3000
+
+const db = new sqlite3.Database('bread.db')
 
 app.set('view engine', 'ejs')
 
@@ -19,9 +16,80 @@ app.use(bodyParser.json())
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('index', { bread })
+    const page = (req.query.page)
+    const limit = 3
+    const offset = (page - 1) * limit
+    db.all('SELECT * FROM bread limit $1 offset $2', [limit, offset], (err, rows) => {
+        if (err) {
+            console.error(err);
+        } else {
+            res.render('index', { bread: rows })
+        }
+    })
 })
 
+app.get('/Add', (req, res) => {
+    res.render('add')
+})
+
+app.post('/Add', (req, res) => {
+    const { String, Integer, Float, Date, Boolean } = req.body
+
+    const query = `INSERT INTO bread(String,Integer,FLoat,Date,Boolean) VALUES (?,?,?,?,?)`
+    const values = [String, Integer, Float, Date, Boolean]
+
+    db.run(query, values, function (err) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.redirect('/')
+        }
+    })
+})
+
+app.get('/hapus/:id', (req, res) => {
+    const id = req.params.id
+    const query = 'DELETE FROM bread WHERE id = ?'
+    const values = [id]
+    db.run(query, [id], function (err) {
+        if (err) {
+            console.log(err)
+
+        } else {
+            res.redirect('/')
+        }
+    })
+})
+
+app.get('/ubah/:id', (req, res) => {
+    const id = req.params.id
+    const query = 'SELECT * FROM bread WHERE id = ?'
+    const values = [id]
+
+    db.get(query, [id], (err, row) => {
+        if (err) {
+            console.error(err)
+        } else {
+            res.render('edit', { item: row })
+        }
+    })
+})
+
+app.post('/ubah/:id', (req, res) => {
+    const id = req.params.id
+    const { String, Integer, Float, Date, Boolean } = req.body
+
+    const query = 'UPDATE bread SET String = ?, Integer = ?, FLoat = ?, Date = ?, Boolean = ? WHERE id = ?';
+    const values = [String, Integer, Float, Date, Boolean, id];
+
+    db.run(query, values, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/')
+        }
+    })
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
--- bread.sql
+++ bread.sql
@@ -1,12 +1,18 @@
-CREATE TABLE data (
+CREATE TABLE bread (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    dataString VARCHAR(255) NOT NULL,
-    dataInteger INTEGER,
-    dataFloat FLOAT,
-    tanggal DATE,
-    dataBoolean BOOLEAN
+    String VARCHAR(255) NOT NULL,
+    Integer INTEGER,
+    Float FLOAT,
+    Date DATE,
+    Boolean BOOLEAN
+
 );
 
-INSERT INTO data(id,dataString,dataInteger,dataFloat,tanggal,dataBoolean)
+INSERT INTO bread(id,String,Integer,Float,Date,Boolean)
 VALUES(1,"rifqi",15,15.15,"2003-05-01","false"),
-(2,"yudi",11,1.15,"2000-01-05","true");
+(2,"gema",19,1.15,"1999-11-12","true"),
+(3,"fahmi",88,19.1,"1995-02-11","false"),
+(4,"gilang",22,2.12,"1997-12-11","true"),
+(5,"yurza",29,2.1,"2000-11-19","false"),
+(6,"ian",77,2.44,"2004-03-11","false"),
+(7,"yudi",11,1.15,"1995-01-05","true");
